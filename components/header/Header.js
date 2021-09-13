a43ccbe9f0124362f0bad2f9262dadf25729eb27
503139218 <marlex.ventura@ge.com>
2021-09-13 21:30:26 +0800

    Add jquery scripts

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,14 +1,16 @@
 // components/header/Header.js
 import NextHead from "next/head"
-  
+
 const Header = () => (
   <NextHead>
     <title>GE | Building a world that works | General Electric</title>
     <link rel="icon" href="/favicon.ico" />
     <script src="https://c.evidon.com/dg/dg.js" companyid="3453"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"></script>
     <link rel="stylesheet" media="all" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
-  </NextHead>  
+  </NextHead>
 );
-  
+
 export default Header;
