a1d0e55ea9b662b9a44a730b3d13ee46c6ebe253
503139218 <marlex.ventura@ge.com>
2021-09-16 22:31:02 +0800

    Fix Build Error

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,5 +1,22 @@
-import * as React from "react";
 import Layout from "../../components/layout/Layout";
 import Link from 'next/link';
 
+
+const BlogTemplate = ({ blog }) => {
+
+  return (
+   <Layout>
+      </section>  
+      <div>
+        <Link href="/blogs">
+          <a>
+            <h6>View all blog posts</h6>
+          </a>
+        </Link>     
+      </div>
+    </Layout>
+  )
+};
+
+export default BlogTemplate
 
