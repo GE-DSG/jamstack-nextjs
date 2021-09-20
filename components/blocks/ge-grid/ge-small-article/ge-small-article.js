1cba248e118f76e1e99ba8090a7cfbe99d5edbd6
Pataiya <703249074@genpact.com>
2021-09-20 13:16:04 +0530

    added image into ge-small-article

--- components/blocks/ge-grid/ge-small-article/ge-small-article.js
+++ components/blocks/ge-grid/ge-small-article/ge-small-article.js
@@ -4,6 +4,7 @@ import {
   BlocksControls,
   InlineTextarea,
   InlineBlocks,
+  InlineGroup,
 } from "react-tinacms-inline";
 
 
@@ -17,8 +18,18 @@ export const GESmallArticle = ({ data, index }) => {
 
     <section className={ styles.geSmallArticle }>
       <div className={styles.cardContainer} >
-        <div className="article-media">
-          <div className="media-bg" style={{ backgroundColor: "#f0f3f7" }}></div>
+        <div className="articleMedia">
+        <InlineGroup
+                    name="image"
+                    focusRing={{ offset: 0, borderRadius: 0 }}
+                    insetControls={true}
+                    fields={IMAGE_FIELDS}
+                  >
+                  <img className={styles.image} 
+                  src={data.image.src}
+                  alt={data.image.alt}
+                  />
+                    </InlineGroup>
         </div>
         <a href="" target="_self" className="">
           <div className="card-stack article-content" >
@@ -39,12 +50,40 @@ export const GESmallArticle = ({ data, index }) => {
   );
 };
 
+export function GESmallArticleBlock({ data, index }) {
+  return (
+    <BlocksControls
+      index={index}
+      focusRing={{ offset: -12 }}
+      insetControls={true}
+    >
+      <GESmallArticle data={data} />
+    </BlocksControls>
+  );
+}
+
+export const IMAGE_FIELDS = [
+  {
+    name: "src",
+    label: "Image Source",
+    component: "text",
+  },
+  {
+    name: "alt",
+    label: "Alt Text",
+    component: "text",
+  },
+];
 
 export const gesmallarticle_template = {
   label: "GE Small Article",
   defaultItem: {
     title: "GE Small Article Title",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley.",
+      image: {
+      src: "../static/ge-small-article.jpg",
+      alt: "General Electric",
+    },
   },
   fields: [
     {
@@ -56,6 +95,12 @@ export const gesmallarticle_template = {
       name: "description",
       label: "Description",
       component: "textarea",
+    },
+    {
+      name: "image",
+      label: "Image",
+      component: "group",
+      fields: IMAGE_FIELDS,
     },
   ],
 };
