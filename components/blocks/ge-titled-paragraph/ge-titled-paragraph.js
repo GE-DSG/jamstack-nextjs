f1b0c9f8d213beba3496d9eae05a581c952ac89e
Pataiya <703249074@genpact.com>
2021-09-23 18:31:46 +0530

    updated the title color

--- components/blocks/ge-titled-paragraph/ge-titled-paragraph.js
+++ components/blocks/ge-titled-paragraph/ge-titled-paragraph.js
@@ -1,30 +1,24 @@
 
 import * as React from "react";
-import { ThemeContext } from "../../utilities/theme";
-import { Section, SectionFields } from "../../utilities/section";
+import { Section } from "../../utilities/section";
 import styles from './ge-titled-paragraph.module.scss';
 import {
   BlocksControls,
-  InlineText,
   InlineTextarea,
 } from "react-tinacms-inline";
 
 
 export const GETitledParagraph = ({ data }) => {
-  const theme = React.useContext(ThemeContext);
-
   return (
-    <Section variant={data.style.color}>
-    <section data-section-id="data-section-id" className={ `${styles.geTitledParagraph}` } style={{ backgroundColor: "transparent" }}>
-      <div className="container-fluid-custom">
+    <Section variant={data.style.color} data-section-id="data-section-id" >    
+      <div className={ `${styles.geTitledParagraph} container-fluid-custom`}>
         <div className="row">      
-          <div className="content-row col-12 col-lg-9">
-            <h5 style={{ color: "var(--ge-primary-blue)", backgroundColor: "transparent", textAlign: "left", fontSize: "20px" }}><InlineTextarea name="title" /></h5>
-            <h2 style={{ color: "var(--ge-dark-blue-grey)", backgroundColor: "transparent", textAlign: "left", fontSize: "42px" }}><InlineTextarea name="content" /></h2>
+          <div className="col-12 col-lg-9">
+            <h5><InlineTextarea name="title"/></h5>
+            <h2><InlineTextarea name="content"/></h2>
           </div>
         </div>
-      </div>
-    </section>  
+      </div>      
     </Section>       
   );
 };
@@ -33,7 +27,7 @@ export function GETitledParagraphBlock({ data, index }) {
   return (
     <BlocksControls
       index={index}
-      focusRing={{ offset:0 }}
+      focusRing={{ offset:-10 }}
       insetControls={true}
     >
       <GETitledParagraph data={data} />
@@ -45,8 +39,7 @@ export const getitledparagraph_template = {
   label: "GE Titled Paragraph",
   defaultItem: {
     title: "OUR MISSION",
-    content: "GE drives the world forward by tackling its biggest challenges, bringing real progress and possibility to every corner of the planet.",  
-    
+    content: "GE drives the world forward by tackling its biggest challenges, bringing real progress and possibility to every corner of the planet.",     
     style: {
       color: "tint",
     },
@@ -61,12 +54,6 @@ export const getitledparagraph_template = {
       name: "content",
       label: "Content",
       component: "textarea",
-    },
-    {
-      name: "",
-      label: "Text",
-      component: "group",
-      fields: [...SectionFields],
     },
     {
       name: "style",
--- components/utilities/section.js
+++ components/utilities/section.js
@@ -4,14 +4,16 @@ import { ThemeContext } from "./theme";
 export const Section = ({ variant = "default", children }) => {
   const theme = React.useContext(ThemeContext);
 
-  let variantClasses = `relative transition duration-150 ease-out text-gray-800 dark:text-gray-100 bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-700 dark:to-gray-800 body-font overflow-hidden`;
+  let variantClasses = `relative transition duration-150 ease-out overflow-hidden`;
 
   if (variant === "tint") {
-    variantClasses = `relative transition duration-150 ease-out text-gray-700 dark:text-gray-100 bg-gradient-to-br from-gray-50 to-white dark:from-gray-900 dark:to-gray-800 body-font overflow-hidden`;
+    variantClasses = `light relative transition duration-150 ease-out overflow-hidden`;
   }
 
   if (variant === "primary") {
-    variantClasses = `relative transition duration-150 ease-out text-gray-700 dark:text-gray-100 bg-${theme.color}-700 bg-gradient-to-br from-${theme.color}-500 to-${theme.color}-300 dark:from-${theme.color}-500 dark:to-${theme.color}-700 body-font overflow-hidden`;
+    variantClasses = `dark relative transition duration-150 ease-out overflow-hidden `;
+    // variantClasses = `relative transition duration-150 ease-out text-gray-700 dark:text-gray-100 bg-${theme.color}-700 bg-gradient-to-br from-${theme.color}-500 to-${theme.color}-300 dark:from-${theme.color}-500 dark:to-${theme.color}-700 body-font overflow-hidden`;
+ 
   }
 
   return <section className={variantClasses}>{children}</section>;
