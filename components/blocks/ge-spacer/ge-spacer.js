d1ed859d41c6ce9d7d311b921662defe0f549d08
Pataiya <703249074@genpact.com>
2021-09-23 20:20:04 +0530

    update

--- components/blocks/ge-spacer/ge-spacer.js
+++ components/blocks/ge-spacer/ge-spacer.js
@@ -13,7 +13,7 @@ import {
 
 export const GESpacer = ({data}) =>{
 return (
-  <Section variant={data.style.color}> 
+  <Section> 
    <div className={styles.GESpacer } style={{ height:`${data.height}px` }} data-section-id="data-section-id">          
      <InlineTextarea
      focusRing={{ offset:0, borderRadius:10}}
