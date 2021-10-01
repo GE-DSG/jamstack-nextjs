b0739f3547ad0c3b8096552751af92fc081eabdb
Pataiya <703249074@genpact.com>
2021-10-01 13:11:51 +0530

    about us page updated

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,26 +1,124 @@
-import * as React from "react";
-import Link from 'next/link'
-import styles from "./page.module.scss"
+// import * as React from "react";
+// import Link from 'next/link'
+// import styles from "./page.module.scss"
 
 //import { getGithubPreviewProps, parseJson } from "next-tinacms-github"
 //import { useGithubJsonForm, useGithubToolbarPlugins } from "react-tinacms-github"
 //import getGlobalStaticProps from "../utils/getGlobalStaticProps"
-import Layout from "/components/layout/Layout"
+// import Layout from "/components/layout/Layout"
 //import ArticleList from '/components/ArticleList'
 
-const AboutUs = props => {
+// const AboutUs = props => {
   
+//   return (
+//     <Layout>
+//       <div className={styles.aboutusPage}>      
+//         <section>
+//         <h2>AboutUs</h2>
+//         <p>coming soon!</p>
+//         </section>
+//       </div>    
+//     </Layout>
+//   )
+// }
+
+
+// export default AboutUs
+
+
+
+import * as React from "react";
+
+import Head from 'next/head'
+import { getGithubPreviewProps, parseJson } from "next-tinacms-github"
+import { useGithubJsonForm, useGithubToolbarPlugins } from "react-tinacms-github"
+
+import getGlobalStaticProps from "../utils/getGlobalStaticProps"
+import Layout from "../components/layout/Layout";
+
+import { useCMS, withTina, useForm, usePlugin } from "tinacms";
+import { InlineForm, InlineBlocks } from "react-tinacms-inline";
+import { Theme } from "../components/utilities/theme";
+import { TinaModal } from "../components/utilities/modal";
+
+import { PAGE_BLOCKS } from "../components/blocks/page_blocks";
+
+//export default function Home({ file, preview }) {
+const AboutUs = ({ file, preview }) => {
+  const formOptions = {
+    label: 'About Page',
+    onSubmit: (values) => {
+      setShowModal(true);
+    },
+  }
+
+  const [showModal, setShowModal] = React.useState(false);
+
+  /*
+   ** Register a JSON Tina Form
+   */
+  const [data, form] = useGithubJsonForm(file, formOptions)
+  usePlugin(form)
+
+  useGithubToolbarPlugins()
+
   return (
     <Layout>
-      <div className={styles.aboutusPage}>      
-        <section>
-        <h2>AboutUs</h2>
-        <p>coming soon!</p>
-        </section>
-      </div>    
+      <InlineForm form={form}>
+        <Theme>
+          <InlineBlocks name="blocks" blocks={PAGE_BLOCKS} />
+        </Theme>
+      </InlineForm>
+      {showModal && (
+        <TinaModal
+          data={data}
+          close={() => {
+            setShowModal(false);
+          }}
+        />
+      )}
     </Layout>
   )
 }
 
+
+
+/**
+ * Fetch data with getStaticProps based on 'preview' mode
+ */
+ export const getStaticProps = async function ({ preview, previewData }) {
+  const global = await getGlobalStaticProps(preview, previewData)
+
+  if (preview) {
+    // get data from github
+    const file = (
+      await getGithubPreviewProps({
+        ...previewData,
+        fileRelativePath: "content/about.json",
+        parse: parseJson,
+      })
+    ).props
+
+    return {
+      props: {
+        ...file,
+        ...global,
+      },
+    }
+  }
+  // render from the file system.
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: "content/about.json",
+        data: (await import("../content/about.json")).default,
+      },
+      ...global,
+    },
+  }
+}
 
 export default AboutUs
