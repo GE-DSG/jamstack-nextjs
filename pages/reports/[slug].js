deed271249808c83ca5ee720dc0aa6e2decbd3e1
503139218 <marlex.ventura@ge.com>
2021-10-04 04:35:21 +0800

    Add modal editing of Report page

--- pages/reports/[slug].js
+++ pages/reports/[slug].js
@@ -6,11 +6,10 @@ import getGlobalStaticProps from "../../utils/getGlobalStaticProps"
 import fs from 'fs'
 import styles from "./reports.module.scss"
 import layoutStyles from "/components/layout/layout.module.scss"
-//import { InlineWysiwyg } from "/components/utilities/InlineWysiwyg"
-//import { InlineWysiwyg } from 'react-tinacms-editor'
 
 import { useCMS, withTina, useForm, usePlugin } from "tinacms"
 import { useGithubJsonForm, useGithubToolbarPlugins } from "react-tinacms-github"
+//import { InlineWysiwyg } from "/components/utilities/InlineWysiwyg"
 
 import {
   InlineForm,
@@ -18,144 +17,9 @@ import {
   BlocksControls,
   InlineText,
   InlineTextarea,
-  InlineGroup
+  InlineGroup,
+  InlineField
 } from "react-tinacms-inline";
-
-
-import { TinaModal } from "/components/utilities/modal";
-
-const ReportTemplate = ({ file, preview }) => {
-
-  const formOptions = {
-    label: 'Report Page',
-    fields: [
-      {
-        name: 'slug',
-        label: 'URL',
-        component: 'text',
-      },
-      {
-        name: 'id',
-        label: 'ID',
-        component: 'text',
-      },
-      {
-        name: 'title',
-        label: 'Title',
-        component: 'text',
-        required: true,
-      },
-      {
-        name: 'hero_image',
-        label: 'Hero image',
-        component: "group",
-        fields: IMAGE_FIELDS,
-      },
-      {
-        name: 'date',
-        label: 'Date',
-        component: 'date',
-        dateFormat: 'MMMM DD, YYYY',
-        timeFormat: false,
-      },
-      {
-        name: 'author',
-        label: 'Author',
-        component: 'text',
-      },
-      {
-        name: 'body',
-        label: 'Body',
-        component: 'html',
-      },
-    ],
-
-    onSubmit: (values) => {
-      setShowModal(true);
-    },
-  }
-
-  const [showModal, setShowModal] = React.useState(false);
-
-  const [data, form] = useGithubJsonForm(file, formOptions)
-  usePlugin(form)
-  useGithubToolbarPlugins()
-
-  return (
-
-    <Layout>
-      <InlineForm form={form}>
-        <section className={styles.reports} >
-
-          <div className="container-fluid-custom">
-            <div className="row">
-              <div className={`${layoutStyles.back}`}>
-                <Link href="/reports">
-                  <a>
-                    <h6> ← Back</h6>
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="container-fluid-custom">
-            <div className={`row ${styles.card}`}>
-              <div className="col-12 pt-3 pb-3 pl-3 pr-3">
-                <div className={styles.heroImage}>
-                  <InlineGroup
-                    name="hero_image"
-                    focusRing={{ offset: 0, borderRadius: 0 }}
-                    insetControls={true}
-                    fields={ IMAGE_FIELDS }
-                  >
-                  <img className={styles.image}
-                  src={data.hero_image.src}
-                  alt={data.hero_image.alt}
-                  />
-                  </InlineGroup>
-                </div>
-                <div className="blog__info">
-                  <h2><InlineTextarea name="title" /></h2>
-                  <h6>{ new Date(data.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: '2-digit' }) }</h6>
-                  <p><InlineTextarea name="author" /></p>
-                  <p>
-                    <div dangerouslySetInnerHTML={{__html: data.body }}/>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="container-fluid-custom">
-            <div className="row">
-              <div className={`${layoutStyles.viewAllItems}`}>
-                <Link href="/reports">
-                  <a>
-                    <h6>View all reports</h6>
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-        </section>
-
-      </InlineForm>
-      {showModal && (
-        <TinaModal
-          data={data}
-          close={() => {
-            setShowModal(false);
-          }}
-        />
-      )}
-    </Layout>
-
-  )
-};
-
-
 
 export const IMAGE_FIELDS = [
   {
@@ -172,6 +36,145 @@ export const IMAGE_FIELDS = [
     component: "text",
   },
 ];
+
+export const REPORT_FIELDS = [
+  {
+    name: 'id',
+    label: 'ID',
+    component: 'text',
+  },
+  {
+    name: 'title',
+    label: 'Title',
+    component: 'text',
+    required: true,
+  },
+  {
+    name: 'hero_image',
+    label: 'Hero image',
+    component: "group",
+    fields: IMAGE_FIELDS,
+  },
+  {
+    name: 'date',
+    label: 'Date',
+    component: 'date',
+    dateFormat: 'MMMM DD, YYYY',
+    timeFormat: false,
+  },
+  {
+    name: 'author',
+    label: 'Author',
+    component: 'text',
+  },
+  {
+    name: 'body',
+    label: 'Body',
+    component: 'html',
+  },
+];
+
+
+const ReportTemplate = ({ file, preview }) => {
+console.log(file);
+  const formOptions = {
+    label: 'Report Page',
+    fields: REPORT_FIELDS,
+  }
+
+  //const cms = useCMS();
+  //const [showModal, setShowModal] = React.useState(false);
+
+  const [data, form] = useGithubJsonForm(file, formOptions)
+  usePlugin(form)
+  useGithubToolbarPlugins()
+
+  return (
+
+    <Layout>
+
+      <InlineForm form={form} >
+
+        <section className={styles.reports} >
+
+          <div className="container-fluid-custom">
+            <div className="row">
+              <div className={`${layoutStyles.back}`}>
+                <Link href="/reports">
+                  <a>
+                    <h6> ← Back</h6>
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="container-fluid-custom">
+            <div className={`row ${styles.card}`}>
+              <InlineGroup
+                  focusRing={{ offset: 20 }}
+                  insetControls={true}
+                  name="."
+                  fields={ REPORT_FIELDS }
+              >
+              <div className="col-12 pt-3 pb-3 pl-3 pr-3">
+                <div className={styles.heroImage}>
+                 <InlineGroup
+                    name="hero_image"
+                    focusRing={{ offset: 0, borderRadius: 10 }}
+                    insetControls={true}
+                    fields={IMAGE_FIELDS}
+                  >
+                  <img className={styles.image}
+                  src={data.hero_image.src}
+                  alt={data.hero_image.alt}
+                  />
+                  </InlineGroup>
+
+                </div>
+                <div className="blog__info">
+                  <h2><InlineText name="title" focusRing={{ offset: 5, borderRadius: 10}} /></h2>
+                  <h6>{ new Date(data.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: '2-digit' }) }</h6>
+                  <p><InlineText name="author" focusRing={{ offset: 5, borderRadius: 10}} /></p>
+                  <p>
+                  {/*<InlineWysiwyg name="body" format="html" >*/}
+                    <div dangerouslySetInnerHTML={{__html: data.body }}/>
+                  {/*</InlineWysiwyg>*/}
+                  </p>
+                </div>
+
+              </div>
+
+              </InlineGroup>
+
+            </div>
+          </div>
+
+
+          <div className="container-fluid-custom">
+            <div className="row">
+              <div className={`${layoutStyles.viewAllItems}`}>
+                <Link href="/reports">
+                  <a>
+                    <h6>View all reports</h6>
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+        </section>
+
+      </InlineForm>
+
+    </Layout>
+
+  )
+};
+
+
+
 
 /**
  * Fetch data with getStaticProps based on 'preview' mode
