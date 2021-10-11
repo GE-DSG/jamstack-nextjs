d6d49f1cf01d8e01a9f52f297eec18a63b3cf450
503139218 <marlex.ventura@ge.com>
2021-10-11 20:00:32 +0800

    Add Custom CKEditor options

--- components/utilities/Editor.js
+++ components/utilities/Editor.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
+//import Editor from 'ckeditor5-custom-build/build/ckeditor';
+//import CKEditor5 from 'ckeditor5-custom-build/build/ckeditor';
 
 //import ImageUpload from '@ckeditor/ckeditor5-image';
 
@@ -10,7 +12,7 @@ import React, { useEffect, useState, useRef } from "react";
 const Editor = (props) => {
   // Solution-2 start
   let editorRef = useRef();
-  const { CKEditor, ClassicEditor } = editorRef.current || {};
+  const { CKEditor, CustomEditor } = editorRef.current || {};
   // --- end
   let [loaded, setLoaded] = useState(false);
 
@@ -18,24 +20,70 @@ const Editor = (props) => {
     // Solution-2 start
     editorRef.current = {
       CKEditor: require("@ckeditor/ckeditor5-react").CKEditor, // v3+
-      ClassicEditor: require("@ckeditor/ckeditor5-build-classic"),      
+      //ClassicEditor: require("@ckeditor/ckeditor5-build-classic"),      
+      CustomEditor: require("ckeditor5-custom-build"),
+      //CKEditor5: require("ckeditor5-custom-build/build/ckeditor").CKEditor5,
     };
     // --- end
 
     setLoaded(true);
   }, []); // run on mounting
 
+  const editorConfiguration = {
+    toolbar: {
+      items: [
+        'heading',
+        '|',
+        'bold',
+        'italic',
+        'underline',
+        '|',
+        'bulletedList',
+        'numberedList',        
+        'indent',        
+        'alignment',
+        '|',
+        'link',        
+        'horizontalLine',
+        'blockQuote',
+        'insertTable',
+        '|',
+        'image',
+        'imageInsert',
+        'imageUpload',
+        'mediaEmbed',
+        'undo',
+        'redo'
+        //'sourceEditing'
+      ]
+    },
+    table: {
+      contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
+    },
+    simpleUpload: {
+      // The URL that the images are uploaded to.
+      uploadUrl: 'http://localhost:3000/static'
+/*
+      // Enable the XMLHttpRequest.withCredentials property.
+      withCredentials: true,
+
+      // Headers sent along with the XMLHttpRequest to the upload server.
+      headers: {
+        'X-CSRF-TOKEN': 'CSRF-Token',
+        Authorization: 'Bearer <JSON Web Token>'
+      }
+*/      
+    }
+  };
+
   if (loaded) {
     return (
       <CKEditor
-        editor={ClassicEditor}
+        //editor={ClassicEditor}
+        editor={CustomEditor}
                
         data={props.data}
-/*        
-        config={{
-          plugins: [ ImageUpload ],
-        }}         
-*/        
+        config={editorConfiguration}
         onReady={(editor) => {
           // You can store the "editor" and use when it is needed.
           //console.log("Editor is ready to use!", editor);
@@ -59,5 +107,7 @@ const Editor = (props) => {
     return <h2> Editor is loading </h2>;
   }
 }
+
+
 
 export default Editor;
